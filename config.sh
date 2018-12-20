2515df3917ee63dd1edacee1ac703b71f8d43de7
Vladimir Tarasov <vt@unistra.ru>
2018-12-20 20:06:55 +0300

    Added config and some scripts

---
+++ config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+URL="https://my.axelname.ru/rest"
+NS1="ns1.domainparking.ru"
+NS2="ns2.domainparking.ru"
+
+FILE="account.sh"
+
+if [ ! -f $FILE ]; then
+    echo "File ${FILE} not found!"
+    echo "Open link: https://my.axelname.ru/client/profile/"
+    read -p "Nic handle: " NIC
+    echo "Open link: https://my.axelname.ru/client/profile/rest/"
+    read -p "API token: " API_KEY
+    echo "Create file: ${FILE}"
+    echo "#!/bin/bash" > $FILE
+    echo "NIC='${NIC}'" >> $FILE
+    echo "API_KEY='${API_KEY}'" >> $FILE
+    chmod a+x $FILE
+    echo "File: ${FILE} crated..."
+    exit
+fi
+
+
